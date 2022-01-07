d9aff2a50c7658b6400770dec1a33e6359ab3ecd
Tin <bachmach24k@gmail.com>
2022-01-06 21:06:59 -0600

    finished basic js

--- runner.js
+++ runner.js
@@ -10,3 +10,52 @@ function computerPlay() {
     return "Scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toUpperCase();
+  let computer= computerSelection.toUpperCase();
+  let playWin;
+
+  if (player == computer) {
+    return "Tie!";
+  }
+  else if (player == "ROCK" && computer == "SCISSORS" ||
+  player == "SCISSORS" && computer == "PAPER" ||
+  player == "PAPER" && computer == "ROCK") {
+    playWin = true;
+  }
+  else {
+    playWin = false;
+  }
+
+  return playWin ? `You win! ${player} beats ${computer}`
+  : `You lose ${computer} beats ${player}`;
+}
+
+function game() {
+  let playerWinCount = 0;
+  let computerWinCount = 0;
+  while (playerWinCount < 3 && computerWinCount < 3) {
+    let player = prompt("Rock, Paper, or Scissors?");
+    let computer = computerPlay();
+    let round = playRound(player, computer);
+  
+    if (round.includes("win")) {
+      playerWinCount++;
+      alert("Player wins this round!")
+      alert(`Score\nPlayer:${playerWinCount}\nComputer:${computerWinCount}`);
+    }
+    else if (round.includes("lose")) {
+      computerWinCount++;
+      alert("Computer wins this round!")
+      alert(`Score\nPlayer:${playerWinCount}\nComputer:${computerWinCount}`);
+    }
+    else {
+      alert("Its a tie!");
+      continue;
+    }
+  }
+  return (playerWinCount > computerWinCount) ? "You win the game!" : "You lose the game!";
+}
+
+game();
